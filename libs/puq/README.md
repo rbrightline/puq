60a62212de6bb6510cf20ba7d24bce2082f08cae
Robert Brightline <robert.brightline@gmail.com>
2025-02-18 11:55:50 -0600

    (test) testing library generator

---
+++ libs/puq/README.md
@@ -0,0 +1,11 @@
+# puq
+
+This library was generated with [Nx](https://nx.dev).
+
+## Building
+
+Run `nx build puq` to build the library.
+
+## Running unit tests
+
+Run `nx test puq` to execute the unit tests via [Vitest](https://vitest.dev/).
---
+++ libs/puq/src/generators/library/library.ts
@@ -0,0 +1,29 @@
+import {
+  addProjectConfiguration,
+  formatFiles,
+  generateFiles,
+  names,
+  Tree,
+} from '@nx/devkit';
+import * as path from 'path';
+import { LibraryGeneratorSchema } from './schema';
+
+export async function libraryGenerator(
+  tree: Tree,
+  options: LibraryGeneratorSchema
+) {
+  const projectRoot = `libs/${options.name}`;
+
+  addProjectConfiguration(tree, options.name, {
+    root: projectRoot,
+    projectType: 'library',
+    sourceRoot: `${projectRoot}/src`,
+    targets: {},
+  });
+  generateFiles(tree, path.join(__dirname, 'files'), projectRoot, {
+    ...names(options.name),
+  });
+  await formatFiles(tree);
+}
+
+export default libraryGenerator;
