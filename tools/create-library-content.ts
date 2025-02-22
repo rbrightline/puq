ddb0f9e5bd73477ee651992413babcd56c53e01f
Robert Brightline <robert.brightline@gmail.com>
2025-02-21 18:19:21 -0600

    (tool) sharing libraries data

---
+++ tools/create-library-content.ts
@@ -0,0 +1,27 @@
+#!/usr/bin/env ts-node
+
+import { readdir, readFile, writeFile } from 'fs/promises';
+import { join } from 'path';
+
+async function run() {
+  const ROOT = join(__dirname, '..', 'libs');
+  const libs = await readdir(ROOT);
+
+  const files = libs.map(async (e) => {
+    const root = join(ROOT, e);
+    const packageJsonPath = join(root, 'package.json');
+    const content = (await readFile(packageJsonPath)).toString();
+    const obj = JSON.parse(content);
+    return { [obj.name]: obj };
+  });
+
+  const result = await Promise.all(files);
+
+  const newFile = result.reduce((p, c) => ({ ...p, c }), {});
+
+  const TARGET_ROOT = join(__dirname, 'content');
+  const CONTENT_JSON_FILE_PATH = join(TARGET_ROOT, 'content.json');
+  await writeFile(CONTENT_JSON_FILE_PATH, JSON.stringify(newFile));
+}
+
+run();
