32282ffd745d91c72da56bfe07ad83a681499eaa
Robert Brightline <robert.brightline@gmail.com>
2025-02-28 21:33:17 -0600

    (publish) publishing

--- libs/names/src/index.ts
+++ libs/names/src/index.ts
@@ -1,4 +1,10 @@
-// @index('./**/index.ts', f => `export * from '${f.path}.js'`)
-export * from './lib/common/index.js';
-export * from './lib/names/index.js';
-export * from './lib/paths/index.js';
+// @index(['./**/*.ts', '!./**/*.{spec,test}.ts','!./**/index.ts'], f => `export * from '${f.path}.js'`)
+export * from './lib/common/normalize.js';
+export * from './lib/names/names-option.js';
+export * from './lib/names/names-type.js';
+export * from './lib/names/names.js';
+export * from './lib/names/pluralize.js';
+export * from './lib/paths/auth-paths-type.js';
+export * from './lib/paths/auth-paths.js';
+export * from './lib/paths/paths-type.js';
+export * from './lib/paths/paths.js';
--- libs/rest/src/index.ts
+++ libs/rest/src/index.ts
@@ -1,3 +1,4 @@
-// @index('./**/index.ts', f => `export * from '${f.path}.js'`)
-export * from './lib/controller/index.js';
-export * from './lib/method/index.js';
+// @index(['./**/*.ts', '!./**/*.{spec,test}.ts','!./**/index.ts'], f => `export * from '${f.path}.js'`)
+export * from './lib/controller/controller.js';
+export * from './lib/method/common.js';
+export * from './lib/method/find-all.js';
