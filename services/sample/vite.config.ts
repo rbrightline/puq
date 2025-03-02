7f3c79c6ac4d9407f78dc5664d1cd50c79d9cfc3
Robert Brightline <robert.brightline@gmail.com>
2025-03-01 20:55:17 -0600

    (service) configured service template

--- services/sample/vite.config.ts
+++ services/sample/vite.config.ts
@@ -3,16 +3,11 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   root: __dirname,
   cacheDir: '../../node_modules/.vite/libs/sample',
-  plugins: [],
-  // Uncomment this if you are using workers.
-  // worker: {
-  //  plugins: [ nxViteTsPaths() ],
-  // },
   test: {
     watch: false,
     globals: true,
     environment: 'node',
-    include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+    include: ['src/**/*.{test,spec}.ts'],
     reporters: ['default'],
     coverage: {
       reportsDirectory: './test-output/vitest/coverage',
